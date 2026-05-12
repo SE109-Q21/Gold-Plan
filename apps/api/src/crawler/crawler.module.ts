241de0e37057f08ea6d9c28db428e8b84e82195a
Nguyễn Tuấn Lộc ( aka Johan) <nguyenloc24022005@gmail.com>
2026-05-12 10:28:14 +0700

    feat: register SJC and DOJI crawlers in CrawlerModule

--- apps/api/src/crawler/crawler.module.ts
+++ apps/api/src/crawler/crawler.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { AnomalyDetectorService } from './anomaly-detector.service';
 import { CrawlSchedulerService } from './crawl-scheduler.service';
+import { SjcCrawlerService } from './sjc-crawler.service';
+import { DojiCrawlerService } from './doji-crawler.service';
 
 @Module({
   imports: [ScheduleModule.forRoot()],
-  providers: [AnomalyDetectorService, CrawlSchedulerService],
+  providers: [
+    AnomalyDetectorService,
+    CrawlSchedulerService,
+    SjcCrawlerService,
+    DojiCrawlerService,
+  ],
   exports: [AnomalyDetectorService, CrawlSchedulerService],
 })
 export class CrawlerModule {}
